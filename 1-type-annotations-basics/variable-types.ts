8f46200838bcf20621e099a11da9ba1d702eeb68
willywdev <werwein.dev@gmail.com>
2023-12-25 14:19:24 +0100

    Learn variable types

---
+++ 1-type-annotations-basics/variable-types.ts
@@ -0,0 +1,8 @@
+// Assiging a basic type to a variable is easy: Just add :Type after the variable name
+
+const myString: string = "Hello World!";
+let movieTitle: string = "Spiderman";
+
+const birthMonth: number = 11;
+
+const myBool: boolean = false;
