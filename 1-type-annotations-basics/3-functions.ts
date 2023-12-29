dd5e17e3241dc72e135a3fbadc681bee18ed687a
willywdev <werwein.dev@gmail.com>
2023-12-29 11:59:07 +0100

    Add type annotations to functions

---
+++ 1-type-annotations-basics/3-functions.ts
@@ -0,0 +1,9 @@
+// In functions we can set the type for the parameters and the return value:
+function square(num: number): number {
+  return num * num;
+}
+
+function greet(name: string): string {
+  name.toUpperCase();
+  return `Hello ${name}`;
+}
