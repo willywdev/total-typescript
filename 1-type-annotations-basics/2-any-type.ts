ec01f689c37062c0866ba9b33fe4cacbd5efacb5
willywdev <werwein.dev@gmail.com>
2023-12-29 11:54:13 +0100

    Add examples of implicit 'any' type

--- 1-type-annotations-basics/2-any-type.ts
+++ 1-type-annotations-basics/2-any-type.ts
@@ -1,2 +1,13 @@
 // 'any' is an escape hatch. It turns off type checking
 const complicatedData: any = "very complicated";
+
+// If a variable is declared without a type, it is implicitly 'any'
+let implicitAny;
+
+// Even after a variable is initialized, it is still implicitly 'any'
+implicitAny = "still any";
+implicitAny = 1;
+implicitAny = true;
+implicitAny = ["anything", "you", "want", 1, true];
+
+// Because of this we try to avoid 'any' as much as possible
